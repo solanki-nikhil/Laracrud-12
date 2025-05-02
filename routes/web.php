1a018f18fae89d3d40fd744bc19624f0adf06f86
nikhil <nikhildndsofware@gmail.com>
2025-05-02 19:00:32 +0530

    read data.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('user',[UserController::class,'index']);
+Route::get('user/create',[UserController::class,'create'])->name('user.create');
