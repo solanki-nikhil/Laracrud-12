d65294a08fadfa4517bcc81c6a934040649aea38
nikhil-solanki <nikhilsolanki02549@gmail.com>
2025-05-12 23:57:47 +0530

    remove all logins

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,3 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('user',[UserController::class,'index']);
-Route::get('user/create',[UserController::class,'create'])->name('user.create');
